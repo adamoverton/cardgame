12e37e3b7d87f86e5e5b62fd6e02c15c6eb3ae71
Adam Overton <adam.overton@gmail.com>
2019-04-07 13:07:13 -0700

    Covert from unionize to Redoodle

--- src/actions/GameActions.tsx
+++ src/actions/GameActions.tsx
@@ -1,20 +1,39 @@
+import { EffectName, StatusEffect } from 'src/GamePlay/Effect';
+import { StoreState } from 'src/types/StoreState';
+import { TypedAction } from "redoodle";
 import { ThunkAction } from 'redux-thunk';
-import { ofType, unionize, UnionOf } from 'unionize';
-import { EffectName } from 'src/GamePlay/Effect';
-import { StoreState, Enemy } from 'src/types/StoreState';
 
-export const Actions = unionize({
-    ADJUST_HP: ofType<{ targetEntityId: string, hp: number }>(),
-    ADJUST_ENERGY: ofType<{ energy: number }>(),
-    SET_ENERGY: ofType<{ energy: number }>(),
-    APPLY_EFFECT: ofType<{ effectName: EffectName, targetId: string, magnitude: number }>(),
-    CLEAR_ENEMIES: ofType<{}>(),
-    ADD_ENEMY: ofType<Partial<Enemy>>(),
-    REMOVE_ENEMY: ofType<string>(),
-}, {
-    tag: 'type',
-    value: 'payload',
-});
+export const AdjustHp = TypedAction.define("gameplay::adjustHp")<{ 
+    targetEntityId: string;
+    hp: number;
+}>();
 
-export type ActionsType = UnionOf<typeof Actions>;
-export type ThunkType = ThunkAction<void, StoreState, void, ActionsType>
+export const AdjustEnergy = TypedAction.define("gameplay::adjustEnergy")<{
+     energy: number; 
+}>();
+
+export const SetEnergy = TypedAction.define("gameplay::setEnergy")<{
+    energy: number;
+}>();
+
+export const ApplyEffect  = TypedAction.define("gameplay::applyEffect")<{
+    effectName: EffectName;
+    targetId: string;
+    magnitude: number;
+}>();
+
+export const ClearEnemies  = TypedAction.define("gameplay::clearEnemies")<{
+}>();
+
+export const AddEnemy  = TypedAction.define("gameplay::addEnemy")<{
+    hp: number;
+    maxHp: number;
+    effectList: StatusEffect[];
+}>();
+
+export const RemoveEnemy  = TypedAction.define("gameplay::removeEnemy")<{
+    id: string;
+}>();
+
+// TODO: Last any was supposed to be union of types...we don't have access to?
+export type ThunkType = ThunkAction<void, StoreState, void, any>
--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,6 +1,6 @@
 import { CardDefinitions, CardName } from './GamePlay/Card';
 import { createStore, applyMiddleware } from 'redux';
-import {reducer} from 'src/reducers/GameReducer';
+import { createReducer } from 'src/reducers/GameReducer';
 import { StoreState } from 'src/types/StoreState';
 import thunk from 'redux-thunk';
 import { EffectName } from 'src/GamePlay/Effect';
@@ -49,7 +49,7 @@ export const defaultState: StoreState = {
 export const configureStore = (initialState?: StoreState) => {
     initialState = initialState || defaultState;
     return createStore<StoreState, any, any, any>(
-        reducer as any,
+        createReducer(),
         initialState,
         applyMiddleware(thunk)
     );
